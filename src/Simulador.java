ef6de45e46d3d594b702e026298ad125595932f5
polioubinha <polioubinha@gmail.com>
2023-09-28 19:17:14 +0200

    Añadido opcions al menu

--- src/Simulador.java
+++ src/Simulador.java
@@ -1,29 +1,30 @@
-import java.util.ArrayList;
+import java.io.Console;
 
-public class Simulador{
+public class Simulador {
     private int numeroDias = 0;
     private int numeroPiscifactorias = 0;
     private String nombreEmpresa = "";
-    
+
     public static void main(String[] args) {
         mostrarMenu();
     }
+
     public int getNumeroDias() {
         return numeroDias;
     }
-    
+
     public void setNumeroDias(int numeroDias) {
         this.numeroDias = numeroDias;
     }
-    
+
     public int getNumeroPiscifactorias() {
         return numeroPiscifactorias;
     }
-    
+
     public void setNumeroPiscifactorias(int numeroPiscifactorias) {
         this.numeroPiscifactorias = numeroPiscifactorias;
     }
-    
+
     public String getNombreEmpresa() {
         return nombreEmpresa;
     }
@@ -48,11 +49,61 @@ public class Simulador{
         System.out.println("12. Mejorar");
         System.out.println("13. Pasar varios días");
         System.out.println("14. Salir");
+
+        Console c = System.console();
+        int opt= Integer.parseInt(c.readLine());
+
+        switch (opt) {
+            case 1:
+                System.out.println();
+                break;
+            case 2:
+                System.out.println();
+                break;
+            case 3:
+                System.out.println();
+                break;
+            case 4:
+                System.out.println();
+                break;
+            case 5:
+                System.out.println();
+                break;
+            case 6:
+                System.out.println();
+                break;
+            case 7:
+                System.out.println();
+                break;
+            case 8:
+                System.out.println();
+                break;
+            case 9:
+                System.out.println();
+                break;
+            case 10:
+                System.out.println();
+                break;
+            case 11:
+                System.out.println();
+                break;
+            case 12:
+                System.out.println();
+                break;
+            case 13:
+                System.out.println();
+                break;
+            case 14:
+                System.out.println("Hasta pronto!");
+                break;
+            default:
+                System.out.println("Opción no válida");
+                mostrarMenu();
+        }
     }
 
-    public static void init(){
+    public static void init() {
         // Este método inicializa el sistema y pide los datos
     }
-    
-}
 
+}
