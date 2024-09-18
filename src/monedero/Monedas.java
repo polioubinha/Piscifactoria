b7d3421ae2f227aa88c7a8466bcdfcb41f1c6136
Poli Oubiña <polioubinha@gmail.com>
2024-09-18 18:22:05 +0200

    Añadida clase Monedas

---
+++ src/monedero/Monedas.java
@@ -0,0 +1,55 @@
+package monedero;
+
+public class Monedas{
+    private int cantidad;
+
+    // Singleton
+    private static Monedas instance=null;
+
+    // Constructor privado para que no se pueda instanciar
+    private Monedas(int cantidad){
+        this.cantidad = cantidad;
+    }
+
+    // Método para obtener la instancia, si no existe, la crea
+    public static Monedas getInstance(){
+        if(instance==null){
+            instance = new Monedas(1000);
+        }
+        return instance;
+    }
+
+    // Método para obtener la cantidad de monedas
+    public int getCantidad(){
+        return cantidad;
+    }
+
+    // Método para establecer la cantidad de monedas
+    public void setCantidad(int cantidad){
+        this.cantidad = cantidad;
+    }
+
+    //Metodo para comprobar si hay se puede realizar la compra con la cantidad indicada
+    public boolean comprobarCompra(int precio){
+        if(cantidad >= precio){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    // Método para realizar la compra, resta el precio de la cantidad de monedas
+    public void compra(int precio){
+        this.cantidad -= precio;
+    }
+
+    // Método para realizar la venta, suma el precio a la cantidad de monedas
+    public void venta(int precio){
+        this.cantidad += precio;
+    }
+
+    // Método para añadir monedas
+    public void añadirMonedas(int cantidad){
+        this.cantidad += cantidad;
+    }
+}
